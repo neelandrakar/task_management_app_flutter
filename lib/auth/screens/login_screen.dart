6cee0b05f30cb27fc0655f45fb904d0705005582
neelandra <neelandrakar@gmail.com>
2024-09-27 02:08:55 +0530

    Initial commit.

---
+++ lib/auth/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginSceen extends StatefulWidget {
+  const LoginSceen({super.key});
+
+  @override
+  State<LoginSceen> createState() => _LoginSceenState();
+}
+
+class _LoginSceenState extends State<LoginSceen> {
+  @override
+  Widget build(BuildContext context) {
+    return LoginSceen();
+  }
+}
