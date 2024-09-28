65dd1bdb8f32a05b8d3e0e5b5d58fb1992312fc6
neelandra <neelandrakar@gmail.com>
2024-09-28 23:52:29 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login page',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -36,4 +36,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
