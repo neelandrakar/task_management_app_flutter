cdc0db434a2eb412545765b425be4633ac44db2a
neelandra <neelandrakar@gmail.com>
2024-09-30 01:32:09 +0530

    Push with index file's setting

---
+++ server/global_variables.js
@@ -0,0 +1,10 @@
+// const accountIcon = 'https://res.cloudinary.com/dhfiapa0x/image/upload/v1704776462/xi5ygmziwc7hvkweq6il.png';
+
+const global_variables = {
+  CLOUDINARY_CLOUD_NAME: 'dhfiapa0x',
+  CLOUDINARY_API_KEY: '176855535218294',
+  CLOUDINARY_API_SECRET: 'TTYyXF3IrFE8kCVjQe9JdxEKMUI',
+  MONGODB_URL: 'mongodb+srv://neelandrakar:Neelandra10@cluster0.y8mvuk5.mongodb.net/'
+};
+
+module.exports = global_variables;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
 console.log("Initiating Server...");
+
+const mongoose = require("mongoose");
+const express = require("express");
+const global_variables = require("./global_variables");
+
+const DB_URL = global_variables.MONGODB_URL;
+const PORT = 3000;
+const app = express();
+
+//middleware
+app.use(express.json());
+
+mongoose.connect(DB_URL)
+.then(()=>{
+    console.log('Connected Successfully');
+})
+.catch((e)=>{
+    console.log(`Error while connecting. Error: ${e}`);
+});
+
+
+app.listen(PORT,'0.0.0.0',function(){
+    console.log(`Connected at ${PORT}`);
+});
