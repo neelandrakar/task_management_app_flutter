f47c4849f81087512bad03e11cf6e9c7d7fcf3ed
neelandra <neelandrakar@gmail.com>
2024-10-15 16:06:54 +0530

    With with voteRouter and authRouter mods and directory changes

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -4,27 +4,6 @@ const auth = require('../../middleware/auth');
 const authRouter = express.Router();
 const axios = require('axios');
 
-authRouter.post('/v1/api/vote-for-palmer', async (req, res) => {
-    try {
-      // Data to be sent in the POST request
-      const data = {
-        nomination: "1CFoGkeELD4kocWDALJr4g"
-      };
-  
-      let i=0;
-      while(i<100000){
-      // Make the POST request
-      const response = await axios.post('https://api.potm.easports.com/public/vote', data);
-      console.log(`Voted ${i} times`);
-      i++;
-      }
-  
-      // Send back the response data to the client
-      res.status(200).json(`Voted for Palmer ${i}th times`);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-  });
 
   authRouter.post('/v1/api/sign-up', async (req, res) => {
     try {
