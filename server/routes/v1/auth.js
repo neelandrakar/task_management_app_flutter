59ae5b6ce8844b15802d76534f112ecd47408be1
neelandra <neelandrakar@gmail.com>
2024-10-16 22:13:43 +0530

    Push with sign up api changes

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -9,36 +9,50 @@ authRouter.post("/v1/api/sign-up", (req, res) => {
     
     const { email, mobno, username, password } = req.body;
 
-    const sql = `SELECT * FROM user_tbl where email_id = "${email}" OR mobno = "${mobno}" OR username = "${username}"`;
-    console.log(sql)
+    const sql_one = `SELECT * FROM user_tbl where email_id = "${email}" OR mobno = "${mobno}"`;
+    const sql_two = `SELECT * FROM user_tbl where username = "${username}"`;
 
-    con.query(sql, (err, results) => {
-      if (err) {
+    con.query(sql_one, (err_one, results_one) => {
+      if (err_one) {
         return res.status(500).json({ 
           success: false,
-          msg: err.message
+          msg: err_one.message
          });
       }
 
-      if(results.length>0){
+      if(results_one.length>0){
 
         let duplicate_with = "NA";
         duplicate_with = mobno==0 ? "email address" : "mobile number";
+
         return res.status(409).json({
           success: false,
           msg: `An account with the same ${duplicate_with} already exists..`
         })
       }
 
+      con.query(sql_two, (err_two, results_two) => {
+        if (err_two) {
+          return res.status(500).json({ 
+            success: false,
+            msg: err_two.message
+          });
+        }
 
+        if(results_two.length>0){
 
-    
+          return res.status(409).json({
+            success: false,
+            msg: `An account with the same username already exists..`
+          })
+        }
 
-      res.status(200).json({
-        success: true,
-        msg: "Ready to be created",
+        res.json("Good to go")
       });
+
     });
+
+
 
   } catch (e) {
     res.status(500).json({ 
