38d102dae5ae26965827ffd7f37aadda364a8e03
neelandra <neelandrakar@gmail.com>
2024-10-17 02:07:14 +0530

    Push with sign up api changes with data insertion

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -2,15 +2,17 @@ const express = require("express");
 const User = require("../../models/user");
 const authRouter = express.Router();
 const con = require("../../mysqlConnection"); // Import MySQL connection
+const bcryptjs = require('bcryptjs');
 
 // Get Users Route
-authRouter.post("/v1/api/sign-up", (req, res) => {
+authRouter.post("/v1/api/sign-up", async (req, res) => {
   try {
     
     const { email, mobno, username, password } = req.body;
 
     const sql_one = `SELECT * FROM user_tbl where email_id = "${email}" OR mobno = "${mobno}"`;
     const sql_two = `SELECT * FROM user_tbl where username = "${username}"`;
+    let hashedPassword = "NA";
 
     con.query(sql_one, (err_one, results_one) => {
       if (err_one) {
@@ -21,6 +23,7 @@ authRouter.post("/v1/api/sign-up", (req, res) => {
       }
 
       if(results_one.length>0){
+        //console.log(results_one);
 
         let duplicate_with = "NA";
         duplicate_with = mobno==0 ? "email address" : "mobile number";
@@ -31,7 +34,7 @@ authRouter.post("/v1/api/sign-up", (req, res) => {
         })
       }
 
-      con.query(sql_two, (err_two, results_two) => {
+      con.query(sql_two, async (err_two, results_two) => {
         if (err_two) {
           return res.status(500).json({ 
             success: false,
@@ -47,7 +50,28 @@ authRouter.post("/v1/api/sign-up", (req, res) => {
           })
         }
 
-        res.json("Good to go")
+        hashedPassword = await bcryptjs.hash(password,8);
+
+        const insert_sql = `INSERT INTO user_tbl (username, mobno, email_id) VALUES ("${username}", "${mobno}", "${email}");`
+        console.log(insert_sql);
+
+        con.query(insert_sql, function (err_three, result) {  
+          
+          if (err_three) {
+            return res.status(500).json({ 
+              success: false,
+              msg: err_two.message
+            });           
+          }
+
+          res.status(201).json({
+            success: true,
+            msg: "User created successfully"
+          });
+      });  
+
+        
+        
       });
 
     });
