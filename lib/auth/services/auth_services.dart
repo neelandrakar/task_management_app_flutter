1e8c4f895877b59b393e76a4f3463b9a98bb68a2
neelandra <neelandrakar@gmail.com>
2024-11-09 23:56:11 +0530

    Push with auth services init

---
+++ lib/auth/services/auth_services.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthServices{
+
+  void login({
+      required BuildContext context,
+      required String input,
+      required String password,
+      required VoidCallback onSuccess
+})async{
+
+    try{
+
+    }catch(e){
+
+    }
+
+
+
+
+
+  }
+}
