af1f046a1aee67fd77733b62597609b599c80a46
neelandra <neelandrakar@gmail.com>
2024-11-30 23:50:56 +0530

    Push with profile screen and router.dart changes

--- lib/profile/screens/profile_screen.dart
+++ lib/profile/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_management_app_flutter/constants/MyColors.dart';
 
 class ProfileScreen extends StatefulWidget {
   static const String routeName = "/profile-screen";
@@ -12,6 +13,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MyColors.boneWhite,
       body: Column(
         children: [
 
